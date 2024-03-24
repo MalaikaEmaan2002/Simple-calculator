880cc9d5199288e82e320f6db27829661b8944b5
malaika.emaan <malaika.emaan2002@gmail.com>
2024-03-24 23:39:21 +0500

    save change

--- index.ts
+++ index.ts
@@ -1,30 +1,26 @@
 #! /usr/bin/env node
 
-import inquirer from "inquirer" ;
+import inquirer from "inquirer";
 
-const answer = await inquirer.prompt ([
-    {message:"Enter First number", type: "number", name: "firstName"},
-    {message: "Enter second number", type: "number", name: "secondName"},
-    {
-        message: "select one of the operator to perform action",
-        type: "list",
-        name: "operator",
-        choices: [ "Addition" , "Substraction", "Multiplication","Division" ],
-},
-
+const answer = await inquirer.prompt([
+  { message: "Enter First number", type: "number", name: "firstName" },
+  { message: "Enter second number", type: "number", name: "secondName" },
+  {
+    message: "select one of the operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Substraction", "Multiplication", "Division"],
+  },
 ]);
 // conditional statement
-if (answer.operator === "Addition")
-{console.log(` ${answer.firstName + answer.secondName}`)
-}
-else if (answer.operator === "Substraction")
-{console.log(`${answer.firstName - answer.secondName}`)
-}
-else if (answer.operator === "Multiplication")
-{console.log(`${answer.firstName * answer.secondName}`)
-}
-else if (answer.operator === "Division")
-{console.log(`${answer.firstName / answer.secondName}`)
-}  else{ 
-    console.log ("please select valid operator")
+if (answer.operator === "Addition") {
+  console.log(` ${answer.firstName + answer.secondName}`);
+} else if (answer.operator === "Substraction") {
+  console.log(`${answer.firstName - answer.secondName}`);
+} else if (answer.operator === "Multiplication") {
+  console.log(`${answer.firstName * answer.secondName}`);
+} else if (answer.operator === "Division") {
+  console.log(`${answer.firstName / answer.secondName}`);
+} else {
+  console.log("please select valid operator");
 }
